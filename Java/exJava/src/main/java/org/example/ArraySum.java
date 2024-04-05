25355002722ef33c5dcdac2020ea1695f798170f
Matheus dos Santos <118857863+theuslinor@users.noreply.github.com>
2024-04-05 17:35:14 -0300

    adicionando exercício de fatorial

--- Java/exJava/src/main/java/org/example/ArraySum.java
+++ Java/exJava/src/main/java/org/example/ArraySum.java
@@ -2,40 +2,41 @@ package org.example;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class ArraySum {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        int smaller = Integer.MAX_VALUE;
-        int max = Integer.MIN_VALUE;
-        int numb = 0;
-        List<Integer> numeros = new ArrayList<>();
+        Scanner input = new Scanner(System.in).useLocale(Locale.US);
 
-        while(numb != -1){
+        double smaller = Double.MAX_VALUE;
+        double max = Double.MIN_VALUE;
+        double numb = 0;
+        List<Double> numeros = new ArrayList<>();
+
+        while(numb != -1) {
             System.out.print("Digite um número ou -1 para parar: ");
-            numb = input.nextInt();
+            numb = input.nextDouble();
 
-            if (numb == -1){
+            if (numb == -1) {
                 break;
             }
-            if (numb < smaller){
+            if (numb < smaller) {
                 smaller = numb;
             }
-            if (numb > max){
+            if (numb > max) {
                 max = numb;
             }
 
             numeros.add(numb);
         }
 
-        Integer sum = numeros.stream().reduce(0, Integer::sum);
-        
+        double sum = numeros.stream().reduce(0.0, Double::sum);
+
         System.out.println("O menor valor da lista é " + smaller);
         System.out.println("O maior valor da lista é " + max);
         System.out.println("Os valores da lista somados são " + sum);
 
+        input.close();
     }
-
 }
-
